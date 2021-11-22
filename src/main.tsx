225668a619bc740b5d3eddd736601fadee20e071
rycont <reactdev@kakao.com>
2021-11-22 09:40:54 +0900

    chore: project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
+import { globalCss } from '../stitches.config'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Onboarding } from './page'
+
+globalCss({
+  ":root": {
+    fontSize: "12px"
+  },
+  body: {
+    fontSize: "4rem"
+  }
+})()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Onboarding />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
